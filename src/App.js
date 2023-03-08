ce159b882d607883c9c5ba589b8709d920b59662
yupseth <cristinayup@gmail.com>
2023-03-08 15:06:48 +0200

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import Card from "./components/Card";
 import FactButton from "./components/FactButton";
 import Footer from "./components/Footer";
 
 function App() {
+  const [displayFact, setDisplayFact] = useState("");
+
+  const fetchFact = () => {
+    fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  fetchFact();
+
   return (
     <div className="app">
       <Card />
